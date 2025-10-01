881d0d31aa19eae1fbbb43e316b44a79da4c2fb0
‪zeyad ashraf‬‏ <84470860+zeyadashraf2002@users.noreply.github.com>
2025-10-02 02:41:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,32 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Nav from "./Components/NavbarComponent";
-import login from "./Pages/Login";
-import register from "./Pages/Register";
-import favorites from "./Pages/Favorites";
-import Home from "./Pages/HomePage";
-import NotFound from "./Pages/NotFoundPage";
-import Details from "./Pages/MovieDetails";
-import { useSelector } from "react-redux";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import MoviesPage from "./Pages/MoviesPage";
-import { LangContext } from "./Components/Context/LangContext";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import MoviesPage from "./pages/MoviesPage";
+import login from "./pages/Login";
+import register from "./pages/Register";
+import favorites from "./pages/Favorites";
+import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
+import { LangContext } from "./context/LangContext";
 
-function App() {
-  const myTheme = useSelector((state) => state.myThemeReducer.theme);
-  const myLang = useSelector((state) => state.myLangReducer.lang);
-
-  // أضفنا useState
-  const [contextLang, setContextLang] = useState("EN");
+export default function App() {
+  const [contextLang, setContextLang] = React.useState("en");
 
   return (
-    <div
-      dir={myLang === "EN" ? "ltr" : "rtl"}
-      className={`${
-        myTheme === "LIGHT" ? "bg-light text-dark" : "bg-dark text-light"
-      }`}
-    >
-      <BrowserRouter>
-        <LangContext.Provider value={{ contextLang, setContextLang }}>
-          <Nav />
-          <Switch>
-            <Route path="movieapp/" component={Home} exact />
-            <Route path="movieapp/movies" component={MoviesPage} exact />
-            <Route path="movieapp/login" component={login} exact />
-            <Route path="movieapp/register" component={register} exact />
-            <Route path="movieapp/fav" component={favorites} exact />
-            <Route path="movieapp/movie/:id" component={Details} exact />
-            <Route component={NotFound} /> {/* fallback */}
-          </Switch>
-        </LangContext.Provider>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter basename="/movieapp">
+      <LangContext.Provider value={{ contextLang, setContextLang }}>
+        <Nav />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/movies" component={MoviesPage} exact />
+          <Route path="/login" component={login} exact />
+          <Route path="/register" component={register} exact />
+          <Route path="/fav" component={favorites} exact />
+          <Route path="/movie/:id" component={Details} exact />
+          <Route component={NotFound} /> {/* fallback */}
+        </Switch>
+      </LangContext.Provider>
+    </BrowserRouter>
   );
 }
-
-export default App;
